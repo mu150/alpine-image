b60054bbf9d5fc57f5c0465f0242e18b72152e3b
mu150 <59873824+mu150@users.noreply.github.com>
2025-06-25 06:43:34 -0400

    Update check-binfmt.sh

--- check-binfmt.sh
+++ check-binfmt.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-apk add wget
+apk add wget qemu-arm qemu-aarch64 qemu-arm-static qemu-aarch64-static
 
 miniroot(){
   local arch=$1
@@ -18,7 +18,10 @@ miniroot(){
 miniroot armhf 3.12.0
 miniroot aarch64 3.12.0
 
-apk add qemu-arm qemu-aarch64
+cp /usr/bin/qemu-arm  arch/armhf/root/usr/bin/
+cp /usr/bin/qemu-arm-static  arch/armhf/root/usr/bin/
+cp /usr/bin/qemu-aarch64  arch/aarch64/root/usr/bin/
+cp /usr/bin/qemu-aarch64-static  arch/aarch64/root/usr/bin/
 
 cp /usr/bin/qemu-arm arch/armhf/root/usr/bin/qemu-arm
 cp /usr/bin/qemu-aarch64 arch/aarch64/root/usr/bin/qemu-aarch64
